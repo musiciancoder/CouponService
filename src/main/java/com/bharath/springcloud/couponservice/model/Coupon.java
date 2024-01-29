51959161d29a7dede2622a1e523584230701f83d
musiciancoder <andresguitarra1980@gmail.com>
2024-01-29 15:47:35 -0300

    model and repository

---
+++ src/main/java/com/bharath/springcloud/couponservice/model/Coupon.java
@@ -0,0 +1,57 @@
+package com.bharath.springcloud.couponservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private BigDecimal discount; //el tipo en la tabla (creada previamente en mysql workbench) es decimal
+    private String expDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+
+
+
+
+
+}
