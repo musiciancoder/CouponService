cb76cc282cc3d9cbc1e6bbb74bcf2902e5b4273c
musiciancoder <andresguitarra1980@gmail.com>
2024-01-29 16:00:06 -0300

    create the restcontroller

---
+++ src/main/java/com/bharath/springcloud/couponservice/model/controllers/CouponRestController.java
@@ -0,0 +1,26 @@
+package com.bharath.springcloud.couponservice.model.controllers;
+
+import com.bharath.springcloud.couponservice.model.Coupon;
+import com.bharath.springcloud.couponservice.repos.CouponRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/couponapi")
+public class CouponRestController {
+
+    @Autowired
+    CouponRepo repo;
+
+    @RequestMapping(value = "/coupons", method = RequestMethod.POST)
+    public Coupon create (Coupon coupon){
+        return repo.save(coupon);
+    }
+
+    @RequestMapping(value = "/coupons/{code}", method = RequestMethod.GET)
+    public Coupon getCoupon (String code){
+        return repo.findByCode(code);
+    }
+}
