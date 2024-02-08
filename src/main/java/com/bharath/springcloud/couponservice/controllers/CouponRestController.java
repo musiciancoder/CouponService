f0afaacd1cc77db23d442d244f67778d6d8440c6
musiciancoder <andresguitarra1980@gmail.com>
2024-02-08 12:21:20 -0300

    test setup

--- src/main/java/com/bharath/springcloud/couponservice/controllers/CouponRestController.java
+++ src/main/java/com/bharath/springcloud/couponservice/controllers/CouponRestController.java
@@ -3,6 +3,7 @@ package com.bharath.springcloud.couponservice.controllers;
 import com.bharath.springcloud.couponservice.model.Coupon;
 import com.bharath.springcloud.couponservice.repos.CouponRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,11 +15,13 @@ public class CouponRestController {
     CouponRepo repo;
 
     @RequestMapping(value = "/coupons", method = RequestMethod.POST)
+    @PreAuthorize("hasRole('ADMIN')") //esto es para test unitarios de spring security
     public Coupon create (@RequestBody Coupon coupon){
         return repo.save(coupon);
     }
 
     @RequestMapping(value = "/coupons/{code}", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('USER','ADMIN')") //esto es para test unitarios de spring security
     public Coupon getCoupon (@PathVariable("code") String code){
         return repo.findByCode(code);
     }
