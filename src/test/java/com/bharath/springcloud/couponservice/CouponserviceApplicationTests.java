e738efd2ee764f15d1a2aebd7983a412aa0e68af
musiciancoder <andresguitarra1980@gmail.com>
2024-02-08 17:36:46 -0300

    testing authentication

--- src/test/java/com/bharath/springcloud/couponservice/CouponserviceApplicationTests.java
+++ src/test/java/com/bharath/springcloud/couponservice/CouponserviceApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -22,11 +23,13 @@ class CouponserviceApplicationTests {
 
 	@Test
 	public void testGetCouponWithoutAuth_Forbidden() throws Exception{
-		mvc.perform(get("/couponapi(coupons/SUPERSALE")).andExpect(status().isForbidden()); //simulamos de obtener un cupon sin autorizacion de roles
+	//	mvc.perform(get("/couponapi(coupons/SUPERSALE")).andExpect(status().isForbidden()); //simulamos de obtener un cupon sin autorizacion de roles. Se usa esta linea o la siguiente segun lo q se desee
+		mvc.perform(get("/couponapi(coupons/SUPERSALE")).andExpect(status().isUnauthorized()); //simulamos de obtener un cupon sin autenticacion (sin habrese logueado). Causa ruido que el nombre del metodo se llame isUnauthorized cuando segun el indio es para autenticacion y no para autorizacion. Se debe usar en conjunto con http.Basic  en metodo configure (no debe ir nada mas en este metodo)
 	}
 
 	@Test
-	@WithMockUser(roles= {"USER"})
+	//@WithMockUser(roles= {"USER"}) //aca mockeamos el usuario desde UserDetails
+	@WithUserDetails("doug@bailey.com")  //aca no mockeamos el usuario desde UserDetails, sino que lo extraemos de él mediante el email
 	public void testGetCouponWithoutAuth_Success() throws Exception{
 		mvc.perform(get("/couponapi(coupons/SUPERSALE")).andExpect(status().isOk()); //simulamos de obtener un cupon con autorizacion de roles
 
@@ -67,7 +70,8 @@ class CouponserviceApplicationTests {
 	public void testCORS() throws Exception {
 		mvc.perform(options("/couponapi/coupons").header("Access-Control-Request-Method","POST").header("Origin",
 				"http://www.bharath.com")).andExpect(status().isOk())
-				.andExpect(header().exists("Access-Control-Allow-Origin"))
+				//debemos tener todos estos (siguientes lineas) en la respuesta
+				.andExpect(header().exists("Access-Control-Allow-Origin")) //Esa cabecera debe estar presente en la respuesta, eso es lo que estamos comprobando .
 				.andExpect(header().string("Access-Control-Allow-Origin","*"))
 				.andExpect(header().exists("Access-Control-Allow-Methods"))
 				.andExpect(header().string("Access-Control-Allow-Methods","POST"));
